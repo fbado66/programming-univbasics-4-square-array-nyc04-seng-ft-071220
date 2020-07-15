2b2824d73d70e9f6c58b3be2280ca60bb7de737c
Franklin Bado <fbado66@gmail.com>
2020-07-15 19:45:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   count = 0 
   new_array = []
   while count < array.length do 
- new_array.push(array[count] *=  array[count])
+  new_array.push(array[count] *=  array[count])
     count += 1 
-    
   end 
    new_array
 end
